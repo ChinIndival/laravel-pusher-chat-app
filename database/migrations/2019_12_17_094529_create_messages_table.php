63bd77568045f82d4bdc6d7ea43945cd4b2f450c
congtinh <congtinh@indival.co.jp>
2019-12-18 11:56:55 +0900

    init project

---
+++ database/migrations/2019_12_17_094529_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('content');
+            $table->integer('id_user_send');
+            $table->integer('id_group');
+            $table->integer('id_user_received')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
