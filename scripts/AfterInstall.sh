8c6c8b3e8d5620d21fbd21b9ded762f8a59ee7ed
congtinh <congtinh@indival.co.jp>
2019-12-20 16:52:01 +0900

    deploy aws

---
+++ scripts/AfterInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+cd /var/www/html/test 
+# 
+# Run composer 
+composer install 
+# 
+# Run artisan commands 
+php /var/www/html/test/artisan migrate:refresh
+ 
+php /var/www/html/test/artisan db:seed 
